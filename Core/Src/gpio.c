eebdae0ed6f7348f2bb376922242d4835cb36ee8
dongdong596 <1279672@qq.com>
2026-06-23 22:17:38 +0800

    ``` feat(project): 更新EtherCAT项目配置并优化GPIO和SPI设置
    修改了.gitignore中的项目名称从EtherCat_Test到EtherCatSSC_Test，
    更新了.mxproject文件中的库文件和Keil配置，添加了调试配置文件，
    并调整了GPIO引脚PA4的初始化状态和速度设置以及SPI1的波特率预分频器。
    
    - .gitignore: 更新项目名称为EtherCatSSC_Test
    - .mxproject: 添加PreviousLibFiles和PreviousUsedKeilFiles配置
    - Core/Src/gpio.c: 将PA4初始状态改为GPIO_PIN_SET，速度设为HIGH
    - Core/Src/spi.c: 将SPI1波特率预分频器改为16分频
    - EtherCatSSC_Test.ioc: 更新GPIO和SPI配置参数
    - MDK-ARM/DebugConfig/: 新增调试配置文件
    ```

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -57,7 +57,7 @@ void MX_GPIO_Init(void)
                           |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 
   /*Configure GPIO pins : PE2 PE3 PE4 PE5
                            PE6 PE7 PE8 PE9
@@ -90,7 +90,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB10 PB11 PB12 PB13
